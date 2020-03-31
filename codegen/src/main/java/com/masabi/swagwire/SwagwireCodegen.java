d9ec71d784a9b2b307ee2a56f82f258f6bcccec7
Colin Vipurs <colin.vipurs@masabi.com>
2020-03-31 07:31:13 +0100

    checkpoint

--- codegen/src/main/java/com/masabi/swagwire/SwagwireCodegen.java
+++ codegen/src/main/java/com/masabi/swagwire/SwagwireCodegen.java
@@ -2,15 +2,17 @@ package com.masabi.swagwire;
 
 import io.swagger.codegen.*;
 import io.swagger.codegen.languages.JavaClientCodegen;
+import io.swagger.codegen.languages.KotlinClientCodegen;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class SwagwireCodegen extends JavaClientCodegen {
+public class SwagwireCodegen extends KotlinClientCodegen {
     private static final String CLASS_PREFIX = "SwagWired";
 
     public SwagwireCodegen() {
         this.supportedLibraries.put("swagwire", "Test client generation");
+        overrideTypeMappings();
     }
 
     @Override
@@ -35,7 +37,15 @@ public class SwagwireCodegen extends JavaClientCodegen {
 
     @Override
     public String toModelName(String name) {
-        return CLASS_PREFIX + super.toModelName(name);
+        if (needsMapping(name)) {
+            return CLASS_PREFIX + super.toModelName(name);
+        } else {
+            return super.toModelName(name);
+        }
+    }
+
+    private boolean needsMapping(String name) {
+        return !name.contains(".");
     }
 
     @Override
@@ -45,7 +55,7 @@ public class SwagwireCodegen extends JavaClientCodegen {
         super.processOpts();
 
         modelTemplateFiles.remove("model.mustache");
-        modelTemplateFiles.put("swagwire/model.mustache", ".java");
+        modelTemplateFiles.put("swagwire/model.mustache", ".kt");
         apiTemplateFiles.remove("api.mustache");
         apiTemplateFiles.put("swagwire/api.mustache", ".java");
 
@@ -55,15 +65,36 @@ public class SwagwireCodegen extends JavaClientCodegen {
         this.supportingFiles.removeIf(f -> !templateFilesToKeep.contains(f.templateFile));
         this.supportingFiles.clear();
         this.apiTestTemplateFiles.clear();
-        String invokerFolder = (this.sourceFolder + '/' + this.invokerPackage).replace(".", "/");
+//        String invokerFolder = (this.sourceFolder + '/' + this.invokerPackage).replace(".", "/");
 
 //        this.supportingFiles.add(new SupportingFile("swagwire/apiresponse.mustache", invokerFolder, "RemoteOperation.java"));
 //        this.supportingFiles.add(new SupportingFile("swagwire/wiremock_api_response.mustache", invokerFolder, "RemoteOperation.java"));
-        this.supportingFiles.add(new SupportingFile("swagwire/apiutils.mustache", invokerFolder, "ApiUtils.java"));
+//        this.supportingFiles.add(new SupportingFile("swagwire/apiutils.mustache", invokerFolder, "ApiUtils.java"));
 //        this.supportingFiles.add(new SupportingFile("swagwire/enum_typeadapterfactory.mustache", invokerFolder, "EnumTypeAdapterFactory.java"));
-        setUseRuntimeException(true);
+//        setUseRuntimeException(true);
 
+        // TODO Make APiUtils inline or just ships as part of code - dunno why we generate it
         this.additionalProperties.put("gson", "true");
-        setJava8Mode(true);
+//        setJava8Mode(true);
+    }
+
+    private void overrideTypeMappings() {
+        // ByteArray translation comes from io.swagger.codegen.DefaultCodegen.getSwaggerType()
+        // we handle ByteArray the same way BigDecimal is handled in io.swagger.codegen.languages.KotlinClientCodegen
+        this.importMapping.put("ByteArray", "kotlin.ByteArray");
+
+        this.typeMapping.put("array", "kotlin.collections.List");
+        this.typeMapping.put("list", "kotlin.collections.List");
+        this.instantiationTypes.put("array", "listOf");
+        this.instantiationTypes.put("list", "listOf");
+
+        // we prefer OffsetDateTime
+        String dateTimeType = "java.time.OffsetDateTime";
+        this.typeMapping.put("date-time", dateTimeType);
+        this.typeMapping.put("date", dateTimeType);
+        this.typeMapping.put("Date", dateTimeType);
+        this.typeMapping.put("DateTime", dateTimeType);
+        this.importMapping.put("DateTime", dateTimeType);
+        this.importMapping.put("LocalDateTime", dateTimeType);
     }
 }
